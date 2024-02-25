44cc8ee8c613a08b3b71e94d45291e73b9d699af
nikasukhishvili <zestafanzaza998@gmail.com>
2024-02-25 20:41:21 +0400

    second commit

---
+++ Day 3/homework/lesson3.py
@@ -0,0 +1,7 @@
+Father = 37
+Mother = 36
+Doggy = 5
+
+print(Doggy+2)
+print(Mother+2)
+print(Father+2)
---
+++ Day 4/homework/lesson4.py
@@ -0,0 +1,6 @@
+Name = input("Enter your first name: ")
+Surname = input("Enter your last name: ")
+print("First name:", Name)
+print("Last name:", Surname)
+
+
