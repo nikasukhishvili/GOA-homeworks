69cbdaa57ff815ac19f8d563230b583574b3d38e
nikasukhishvili <zestafanzaza998@gmail.com>
2024-04-04 19:09:39 +0400

    second commit

---
+++ Day 17/homework/lesson17.py
@@ -0,0 +1,13 @@
+def function(string):
+   return (string.lower(), string.upper(), string.capitalize(), string.find('goa'))
+print (function("goa"))
+
+def modifyandjoin(names_list):
+    change = int(input("Which index do you want to change? "))
+    desired_string = input("Enter your desired string: ")
+    names_list[change] = desired_string
+    return ' '.join(names_list)
+
+names = ["name1", "name2", "name3"]
+modified_names = modifyandjoin(names)
+print(modified_names)
